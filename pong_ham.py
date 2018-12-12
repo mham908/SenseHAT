5fc118dc998c707183c4bfa703fcc3aacf97bbed
mham908 <mham908@lsr7.net>
2018-12-12 13:48:42 -0500

    updates

--- pong_ham.py
+++ pong_ham.py
@@ -1,4 +1,5 @@
 from sense_hat import SenseHat
+from time import sleep
 sense = SenseHat()
 
 white = (255, 255, 255)
@@ -9,7 +10,8 @@ def draw_bat():
     sense.set_pixel(0, bat_y, white)
     sense.set_pixel(0, bat_y + 1, white)
     sense.set_pixel(0, bat_y - 1, white)
-draw_bat()
+
+
     
 
 def move_up(event):
@@ -18,3 +20,10 @@ def move_up(event):
         bat_y -= 1
 
 sense.stick.direction_up = move_up
+
+sense.clear(0,0,0)
+while True:
+    sleep(.25) 
+    sense.clear(0,0,0)
+    draw_bat()
+    
