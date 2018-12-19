fab88831150417f44182136f946cddf57866cf6c
Mason Ham <mham908@lsr7.net>
2018-12-19 11:13:19 -0600

    update

--- pong_ham.py
+++ pong_ham.py
@@ -5,12 +5,24 @@ sense = SenseHat()
 
 white = (255, 255, 255)
 purple = (255, 0, 255)
-red = (255,0,0)
+r = (255,0,0)
+e = (0, 0, 0)
 
 bat_y = 4
 ball_position = [3, 3]
 ball_velocity = [1, 1]
+counter = 0
 
+one = [
+    e,e,e,r,r,e,e,e,
+    e,e,r,e,r,e,e,e,
+    e,r,e,e,r,e,e,e,
+    e,e,e,e,r,e,e,e,
+    e,e,e,e,r,e,e,e,
+    e,e,e,e,r,e,e,e,
+    e,r,r,r,r,r,r,e,
+    e,e,e,e,e,e,e,e]
+    
 def draw_ball():
     sense.set_pixel(ball_position[0], ball_position[1], purple)
     ball_position[0] += ball_velocity[0]
@@ -23,7 +35,9 @@ def draw_ball():
         ball_velocity[0] = -ball_velocity[0]
     if ball_position[0] == 0:
         sense.show_message ("Failure.exe")
- 
+        sense.set_rotation(90)
+        sleep(1)
+       
 def draw_bat():
     sense.set_pixel(0, bat_y, white)
     sense.set_pixel(0, bat_y + 1, white)
@@ -39,21 +53,18 @@ def move_up(event):
     global bat_y
     if event.action == 'pressed' and bat_y > 1:
         bat_y -= 1
+        
+        
 
 
 
     
 sense.clear(0,0,0)
 while True:
-    sleep(.000001)
+    sleep(.5)
     sense.clear(0,0,0)
     draw_bat()
     draw_ball()
     sense.stick.direction_down = move_down
     sense.stick.direction_up = move_up
-    sense.set_rotation(90)
-    sleep(.000001)
-    sense.set_rotation(180)
-    sleep(.000001)
-    sense.set_rotation(270)
-    sleep(.000001)
+
